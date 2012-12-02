1dab5423620afe9964c282733cc321212c7f4a9f
Javier Garcia <jagarciaxx@gmail.com>
2012-12-02 21:12:31 +0100

    Primer commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: javier
+ *
+ * Created on 2 de diciembre de 2012, 21:10
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
